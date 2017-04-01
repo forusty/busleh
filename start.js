fb04225588a0a61f1d67b9e67c3fcc1079252cbb
CYJ <ycheng4@e.ntu.edu.sg>
2017-04-02 00:18:29 +0800

    add in initial sg

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
-var TelegramBot = require('node-telegram-bot-api');
-
+var TelegramBot = require('node-telegram-bot-api'),
+    https = require('https');
 var token = '***REMOVED***';
 // Setup polling way
 var bot = new TelegramBot(token, {polling: true});
@@ -8,8 +8,48 @@ bot.onText(/\/busStop (.+)/, function(msg, match) {
   var chatId = msg.chat.id;
   var resp = match[1];
   
+  /**
+  * HOW TO Make an HTTP Call - GET
+  */
+  // options for GET
+  var options = {
+    host : 'http://datamall2.mytransport.sg', // here only the domain name
+    port: 80,
+    path : '/ltaodataservice/BusArrival?BusStopID='+resp', // the rest of the url with parameters if needed
+    method: 'GET',
+    headers: {
+      'AccountKey': 'vHGF3vL5ShOQZ4iknswCqQ',
+    }
+  };
+
+  var responseData="";
+  // do the GET request
+  var reqGet = https.request(options, function(res) {
+      // uncomment it for header details
+      res.on('data', function(d) {
+          console.info('GET result:\n');
+          responseData = d;
+          console.info('\n\nCall completed');
+      });
+
+  });
+
+  reqGet.on('error', function(e) {
+      console.error(e);
+  });
+
+  reqGet.end();
+
+  var busObj = "";
+  for(var i = 0;i<responseData['Services'].length;i++)
+  {
+    var bus = responseData['Services'][i];
+
+    busObj.="============================="
+    busObj.="Service Number :"+bus['ServiceNo']+"\n";
+  }
   // photo can be: a file path, a stream or a Teleram file_id
-  bot.sendMessage(chatId,"You typed Bus number : "+resp);
+  bot.sendMessage(chatId,busObj);
 });
 
 // Matches /love
