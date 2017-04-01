842febf91092ab2de559f56118ec2853916d818f
CYJ <ycheng4@e.ntu.edu.sg>
2017-04-01 23:53:18 +0800

    first commit

--- start.js
+++ start.js
@@ -1,40 +1,13 @@
-const cluster = require('cluster'),
-      stopSignals = [
-        'SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-        'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-      ],
-      production = process.env.NODE_ENV == 'production';
+var TelegramBot = require('node-telegram-bot-api');
 
-let stopping = false;
+var token = '***REMOVED***';
+// Setup polling way
+var bot = new TelegramBot(token, {polling: true});
 
-cluster.on('disconnect', function(worker) {
-  if (production) {
-    if (!stopping) {
-      cluster.fork();
-    }
-  } else {
-    process.exit(1);
-  }
+bot.onText(/\/bus (.+)/, function(msg, match) {
+  var chatId = msg.chat.id;
+  var resp = match[1];
+  
+  // photo can be: a file path, a stream or a Teleram file_id
+  bot.sendMessage(chatId,"You typed Bus number : "+resp);
 });
-
-if (cluster.isMaster) {
-  const workerCount = process.env.NODE_CLUSTER_WORKERS || 4;
-  console.log(`Starting ${workerCount} workers...`);
-  for (let i = 0; i < workerCount; i++) {
-    cluster.fork();
-  }
-  if (production) {
-    stopSignals.forEach(function (signal) {
-      process.on(signal, function () {
-        console.log(`Got ${signal}, stopping workers...`);
-        stopping = true;
-        cluster.disconnect(function () {
-          console.log('All workers stopped, exiting.');
-          process.exit(0);
-        });
-      });
-    });
-  }
-} else {
-  require('./app.js');
-}
