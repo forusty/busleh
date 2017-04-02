dbdd283927d6241a411d0b64d3bbf168ac8e47be
CYJ <ycheng4@e.ntu.edu.sg>
2017-04-02 09:13:14 +0800

    added in timings and bold text

--- start.js
+++ start.js
@@ -1,74 +1,89 @@
 var TelegramBot = require('node-telegram-bot-api'),
     http = require('http');
-    
+
 var token = '***REMOVED***';
 // Setup polling way
-// var bot = new TelegramBot(token, {
-//     polling: true
-// });
+var bot = new TelegramBot(token, {
+    polling: true
+});
 
-// bot.onText(/\/bus_stop (.+)/, function(msg, match) {
-//     var chatId = msg.chat.id;
-//     var resp = match[1];
+bot.onText(/\/bus_stop (.+)/, function(msg, match) {
+    var chatId = msg.chat.id;
+    var resp = match[1];
 
-//     console.log(resp);
+    console.log(resp);
 
-//     /**
-//      * HOW TO Make an HTTP Call - GET
-//      */
-//     // options for GET
-//     var options = {
-//         host: 'datamall2.mytransport.sg', // here only the domain name
-//         port: 80,
-//         path: '/ltaodataservice/BusArrival?BusStopID=' + resp, // the rest of the url with parameters if needed
-//         method: 'GET',
-//         headers: {
-//             AccountKey: '***REMOVED***'
-//         }
-//     };
+    /**
+     * HOW TO Make an HTTP Call - GET
+     */
+    // options for GET
+    var options = {
+        host: 'datamall2.mytransport.sg', // here only the domain name
+        port: 80,
+        path: '/ltaodataservice/BusArrival?BusStopID=' + resp, // the rest of the url with parameters if needed
+        method: 'GET',
+        headers: {
+            AccountKey: '***REMOVED***'
+        }
+    };
 
-//     var responseData = "";
-//     console.log("Starting Get");
-//     // do the GET request
-//     var reqGet = http.request(options, function(res) {
-//         console.log("statusCode: ", res.statusCode);
-//         // uncomment it for header details
-//         res.on('data', function(d) {
-//             console.info('GET result:\n');
-//             responseData += d;
-//         });
+    var responseData = "";
+    console.log("Starting Get");
+    // do the GET request
+    var reqGet = http.request(options, function(res) {
+        console.log("statusCode: ", res.statusCode);
+        // uncomment it for header details
+        res.on('data', function(d) {
+            console.info('GET result:\n');
+            responseData += d;
+        });
 
-//         res.on('end', function() {
-//             // console.log(req.data);
-//             console.log(responseData);
-//             responseData = JSON.parse(responseData);
-//             var busObj = "";
-//             for (var i = 0; i < responseData['Services'].length; i++) {
-//                 var bus = responseData['Services'][i];
+        res.on('end', function() {
+            // console.log(req.data);
+            console.log(responseData);
+            responseData = JSON.parse(responseData);
+            var busObj = "";
+            for (var i = 0; i < responseData['Services'].length; i++) {
+                var bus = responseData['Services'][i];
 
-//                 busObj += "=============================\n"
-//                 busObj += "Service Number :" + bus['ServiceNo'] + "\n";
-                
-//                 // var nextBus = bus['NextBus'];
-//                 // console.log(nextBus['EstimatedArrival']);
-//                 // console.log(new Date().getTime());
-//                 // console.log(new Date().getTime());
-//             }
-//             // photo can be: a file path, a stream or a Teleram file_id
-//             bot.sendMessage(chatId, busObj);
-//             console.info('\n\nCall completed');
-        
-//             // your code here if you want to use the results !
-//         });
+                busObj += "=============================\n"
+                busObj += "*Service Number : " + bus['ServiceNo'] + "*\n";
 
-//     });
+                if (bus['Status'] === 'In Operation') {
+                    // first bus
+                    var nextBus = bus['NextBus'];
+                    var minutues = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
+                    busObj += "Next Bus :" + minutues + " minutes\n";
 
-//     reqGet.on('error', function(e) {
-//         console.error(e);
-//     });
+                    // second bus
+                    nextBus = bus['SubsequentBus'];
+                    minutues = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
+                    busObj += "Next Bus :" + minutues + " minutes\n";
 
-//     reqGet.end();
-// });
+                    // third bus
+                    nextBus = bus['SubsequentBus3'];
+                    minutues = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
+                    busObj += "Next Bus :" + minutues + " minutes\n";
+                } else {
+                    busObj += "Bus not in service";
+                }
+                busObj+="\n";
+            }
+            // photo can be: a file path, a stream or a Teleram file_id
+            bot.sendMessage(chatId, busObj,{"parse_mode":"Markdown"});
+            console.info('\n\nCall completed');
+
+            // your code here if you want to use the results !
+        });
+
+    });
+
+    reqGet.on('error', function(e) {
+        console.error(e);
+    });
+
+    reqGet.end();
+});
 
 // // Matches /love
 // bot.onText(/\/love/, function onLoveText(msg) {
@@ -83,3 +98,13 @@ var token = '***REMOVED***';
 //     };
 //     bot.sendMessage(msg.chat.id, 'Do you love me?', opts);
 // });
+
+function timeDiff(nowMili, busMili) {
+    var diff = busMili - nowMili;
+    if (diff <= 0) {
+        diff = "Now";
+    } else {
+        diff = diff / 1000 / 60
+    }
+    return Math.round(diff);
+}
