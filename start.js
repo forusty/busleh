2e50cca68203fc5e1665d65b6a12f81226f091d0
CYJ <ycheng4@e.ntu.edu.sg>
2017-04-03 23:51:43 +0800

    added in new features and /help

--- start.js
+++ start.js
@@ -3,83 +3,177 @@ var bot = require('./telegrambot.js'),
 
 // check user login
 bot.getBot(function(telegramBot) {
-    telegramBot.onText(/\/bus_stop (.+)/, function(msg, match) {
+    telegramBot.onText(/\/help/, function(msg) {
+        var chatId = msg.chat.id;
+
+        var help ="SG Bus Leh Telegram Bot\n\n";
+        help +="/list <bus stop number> - Get a list of bus at bus stop number with their arrival timings. eg. /list 22341 \n\n";
+        help +="<bus stop number> <bus number> - Get specified bus at the bus stop. eg.22341 242 \n\n";
+        help +="Future enhancement - /send_location to get bus stop number and bus at given gps location\n";
+
+        console.log(help);
+        console.log(msg.text);
+        telegramBot.sendMessage(chatId, help);
+        console.info('Call completed for /help\n');
+    });
+
+    telegramBot.onText(/\/list (.+)/, function(msg, match) {
         var chatId = msg.chat.id;
         var resp = match[1];
 
         console.log(resp);
-
-        /**
-         * HOW TO Make an HTTP Call - GET
-         */
-        // options for GET
-        var options = {
-            host: 'datamall2.mytransport.sg', // here only the domain name
-            port: 80,
-            path: '/ltaodataservice/BusArrival?BusStopID=' + resp, // the rest of the url with parameters if needed
-            method: 'GET',
-            headers: {
-                AccountKey: '***REMOVED***'
-            }
-        };
-
-        var responseData = "";
-        console.log("Starting Get");
-        // do the GET request
-        var reqGet = http.request(options, function(res) {
-            console.log("statusCode: ", res.statusCode);
-            // uncomment it for header details
-            res.on('data', function(d) {
-                console.info('GET result:\n');
-                responseData += d;
+        sgBus(resp, function(busObj) {
+            // photo can be: a file path, a stream or a Teleram file_id
+            telegramBot.sendMessage(chatId, busObj, {
+                "parse_mode": "Markdown"
             });
-
-            res.on('end', function() {
-                // console.log(req.data);
-                console.log(responseData);
-                responseData = JSON.parse(responseData);
-                var busObj = "";
-                for (var i = 0; i < responseData['Services'].length; i++) {
-                    var bus = responseData['Services'][i];
-                    busObj += "=============================\n"
-                    busObj += "*Service Number : " + bus['ServiceNo'] + "*\n";
-
-                    if (bus['Status'] === 'In Operation') {
-                        // first bus
-                        var nextBus = bus['NextBus'];
-                        var minutes = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
-                        busObj += "Next Bus : ";
-                        busObj += minutes <= 1 ? "now\n" : minutes + " minutes\n";
-
-                        // second bus
-                        nextBus = bus['SubsequentBus'];
-                        minutes = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
-                        busObj += "Next Bus : ";
-                        busObj += minutes <= 1 ? "now\n" : minutes + " minutes\n";
-
-                        // third bus
-                        nextBus = bus['SubsequentBus3'];
-                        minutes = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
-                        busObj += "Next Bus : ";
-                        busObj += minutes <= 1 ? "now\n" : minutes + " minutes\n";
-                    } else {
-                        busObj += "Bus not in service";
-                    }
-                    busObj += "\n";
-                }
-                // photo can be: a file path, a stream or a Teleram file_id
-                telegramBot.sendMessage(chatId, busObj, {"parse_mode": "Markdown"});
-                console.info('\n\nCall completed');
-            });
+            console.info('\n\nCall completed for /list');
         });
-
-        reqGet.on('error', function(e) {
-            console.error(e);
-        });
-
-        reqGet.end();
     });
-});
+
+    telegramBot.on("message", function(msg) {
+        var chatId = msg.chat.id;
+        var res = msg.text;
+        console.log(res);
+
+        if (res.substring(0, 1) !== '/') {
+            console.log(res);
+
+            var resArr = res.split(" ");
+
+            console.log(resArr);
+
+            if (resArr.length === 2) {
+                sgBus(resArr[0], function(busObj) {
+                    // photo can be: a file path, a stream or a Teleram file_id
+                    telegramBot.sendMessage(chatId, busObj, {
+                        "parse_mode": "Markdown"
+                    });
+                    console.info('\n\nCall completed for all message');
+                }, resArr[1]);
+            }
+        }
+    });
+}, process.argv[2]);
+
+function sgBus(busStopID, callBack, serviceNo) {
+    /**
+     * HOW TO Make an HTTP Call - GET
+     */
+    //
+    var path = "/ltaodataservice/BusArrival";
+    if (typeof busStopID !== "undefined") {
+        path += '?BusStopID=' + busStopID
+    }
+
+    if (typeof serviceNo !== "undefined") {
+        path += '&ServiceNo=' + serviceNo
+    }
+    console.log("Path : " + path);
+    // options for GET
+    var options = {
+        host: 'datamall2.mytransport.sg', // here only the domain name
+        port: 80,
+        path: path, // the rest of the url with parameters if needed
+        method: 'GET',
+        headers: {
+            AccountKey: '***REMOVED***'
+        }
+    };
+
+    var responseData = "";
+    console.log("Starting Get");
+
+    // do the GET request
+    var reqGet = http.request(options, function(res) {
+        console.log("statusCode: ", res.statusCode);
+        // uncomment it for header details
+        res.on('data', function(d) {
+            console.info('GET result:\n');
+            responseData += d;
+        });
+
+        res.on('end', function() {
+            // console.log(req.data);
+            console.log(responseData);
+            responseData = JSON.parse(responseData);
+            generateBusObj(responseData, callBack);
+        });
+    });
+
+    reqGet.on('error', function(e) {
+        console.error(e);
+    });
+
+    reqGet.end();
+}
+
+function generateBusObj(responseData, callBack) {
+    var busObj = "";
+    if (responseData['Services'].length > 0) {
+        for (var i = 0; i < responseData['Services'].length; i++) {
+            var bus = responseData['Services'][i];
+            busObj += "*Service Number : " + bus['ServiceNo'] + "*\n";
+            busObj += "=============================\n"
+
+            if (bus['Status'] === 'In Operation') {
+                // first bus
+                var nextBus = bus['NextBus'];
+                busObj += returnBusObj(bus['NextBus']);
+                busObj += returnBusObj(bus['SubsequentBus']);
+                busObj += returnBusObj(bus['SubsequentBus3']);
+                // var minutes = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
+                // busObj += "Next Bus : ";
+                // busObj += minutes <= 1 ? "Arr\n" : minutes + " minutes\n";
+                // busObj += " ( " + nextBus['Load'] + " " + returnEmoji(nextBus['Load']) + " )\n";
+
+                // second bus
+                // nextBus = bus['SubsequentBus'];
+                // minutes = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
+                // busObj += "Next Bus : ";
+                // busObj += minutes <= 1 ? "Arr\n" : minutes + " minutes\n";
+                // busObj += " ( " + nextBus['Load'] + " " + returnEmoji(nextBus['Load']) + " )\n";
+
+                // third bus
+                // nextBus = bus['SubsequentBus3'];
+                // minutes = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
+                // busObj += "Next Bus : ";
+                // busObj += minutes <= 1 ? "Arr\n" : minutes + " minutes\n";
+                // busObj += " ( " + nextBus['Load'] + " " + returnEmoji(nextBus['Load']) + " )\n";
+            } else {
+                busObj += "Bus not in service";
+            }
+            busObj += "\n";
+        }
+    } else {
+        busObj = "Invalid Bus Number";
+    }
+    callBack(busObj);
+}
+
+function returnEmoji(load) {
+    var emoji = "";
+    if (load === 'Seats Available') {
+        emoji = "\u{1F604}"
+    } else if (load === 'Standing Available') {
+        emoji = "\u{1F605}"
+    } else if (load === 'Limited Standing') {
+        emoji = "\u{1F630}"
+    }
+    return emoji;
+}
+
+function returnBusObj(nextBus) {
+    var busObj = "Next Bus : ";
+    if (nextBus['EstimatedArrival'] !== "") {
+        minutes = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
+        busObj += minutes <= 1 ? "Arr" : minutes + " minutes";
+        busObj += " (" + nextBus['Load'] + " " + returnEmoji(nextBus['Load']) + ")\n";
+    } else {
+        busObj += "No Bus\n";
+    }
+    return busObj;
+}
 
 function timeDiff(NowMili, busMili) {
     var diff = busMili - NowMili;
--- telegrambot.js
+++ telegrambot.js
@@ -5,34 +5,38 @@ module.exports = {
      * gets all users
      * @param callback
      */
-    getBot: function(callback) {
-        const TOKEN = process.env.TELEGRAM_TOKEN || '***REMOVED***';
-        // See https://developers.openshift.com/en/node-js-environment-variables.html
-        const options = {
-            webHook: {
-                port: process.env.OPENSHIFT_NODEJS_PORT,
-                host: process.env.OPENSHIFT_NODEJS_IP,
-                // you do NOT need to set up certificates since OpenShift provides
-                // the SSL certs already (https://<app-name>.rhcloud.com)
-            },
-        };
+    getBot: function(callback,mode) {
+    	var bot;
+    	if(mode !=='dev')
+    	{
+	        const TOKEN = process.env.TELEGRAM_TOKEN || '***REMOVED***';
+	        // See https://developers.openshift.com/en/node-js-environment-variables.html
+	        const options = {
+	            webHook: {
+	                port: process.env.OPENSHIFT_NODEJS_PORT,
+	                host: process.env.OPENSHIFT_NODEJS_IP,
+	                // you do NOT need to set up certificates since OpenShift provides
+	                // the SSL certs already (https://<app-name>.rhcloud.com)
+	            },
+	        };
 
-        // OpenShift routes from port :443 to OPENSHIFT_NODEJS_PORT
-        const domain = process.env.OPENSHIFT_APP_DNS;
-        const url = `${domain}:443`;
-        const bot = new TelegramBot(TOKEN, options);
+	        // OpenShift routes from port :443 to OPENSHIFT_NODEJS_PORT
+	        const domain = process.env.OPENSHIFT_APP_DNS;
+	        const url = `${domain}:443`;
+	       	bot = new TelegramBot(TOKEN, options);
 
-
-        // This informs the Telegram servers of the new webhook.
-        // Note: we do not need to pass in the cert, as it already provided
-        bot.setWebHook(`${url}/bot${TOKEN}`);
-
-        
-  //       var token = '***REMOVED***';
-		// // Setup polling way
-		// var bot = new TelegramBot(token, {
-		//     polling: true
-		// });
+	        // This informs the Telegram servers of the new webhook.
+	        // Note: we do not need to pass in the cert, as it already provided
+	        bot.setWebHook(`${url}/bot${TOKEN}`);
+    	}
+    	else{
+    		console.log("DEV MODE");
+	        var token = '***REMOVED***';
+			// Setup polling way
+			bot = new TelegramBot(token, {
+			    polling: true
+			});
+    	}
 		callback(bot);
     }
 };
