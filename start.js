84f0921938fe1057fdfb57a10c7588fa13e8af69
CYJ <ycheng4@e.ntu.edu.sg>
2017-04-03 21:21:21 +0800

    testing webhook

--- start.js
+++ start.js
@@ -1,91 +1,90 @@
-var TelegramBot = require('node-telegram-bot-api'),
+var bot = require('./telegrambot.js'),
     http = require('http');
 
-var token = '***REMOVED***';
-// Setup polling way
-var bot = new TelegramBot(token, {
-    polling: true
-});
+// check user login
+bot.getBot(function(telegramBot) {
+    telegramBot.onText(/\/bus_stop (.+)/, function(msg, match) {
+        var chatId = msg.chat.id;
+        var resp = match[1];
 
-bot.onText(/\/bus_stop (.+)/, function(msg, match) {
-    var chatId = msg.chat.id;
-    var resp = match[1];
+        console.log(resp);
 
-    console.log(resp);
-
-    /**
-     * HOW TO Make an HTTP Call - GET
-     */
-    // options for GET
-    var options = {
-        host: 'datamall2.mytransport.sg', // here only the domain name
-        port: 80,
-        path: '/ltaodataservice/BusArrival?BusStopID=' + resp, // the rest of the url with parameters if needed
-        method: 'GET',
-        headers: {
-            AccountKey: '***REMOVED***'
-        }
-    };
-
-    var responseData = "";
-    console.log("Starting Get");
-    // do the GET request
-    var reqGet = http.request(options, function(res) {
-        console.log("statusCode: ", res.statusCode);
-        // uncomment it for header details
-        res.on('data', function(d) {
-            console.info('GET result:\n');
-            responseData += d;
-        });
-
-        res.on('end', function() {
-            // console.log(req.data);
-            console.log(responseData);
-            responseData = JSON.parse(responseData);
-            var busObj = "";
-            for (var i = 0; i < responseData['Services'].length; i++) {
-                var bus = responseData['Services'][i];
-
-                busObj += "=============================\n"
-                busObj += "*Service Number : " + bus['ServiceNo'] + "*\n";
-
-                if (bus['Status'] === 'In Operation') {
-                    // first bus
-                    var nextBus = bus['NextBus'];
-                    var minutes = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
-                    busObj += "Next Bus : ";
-                    busObj += minutes<=1 ? "now\n" : minutes + " minutes\n";
-
-                    // second bus
-                    nextBus = bus['SubsequentBus'];
-                    minutes = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
-                    busObj += "Next Bus : ";
-                    busObj += minutes<=1 ? "now\n" : minutes + " minutes\n";
-
-                    // third bus
-                    nextBus = bus['SubsequentBus3'];
-                    minutes = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
-                    busObj += "Next Bus : ";
-                    busObj += minutes<=1 ? "now\n" : minutes + " minutes\n";
-                } else {
-                    busObj += "Bus not in service";
-                }
-                busObj+="\n";
+        /**
+         * HOW TO Make an HTTP Call - GET
+         */
+        // options for GET
+        var options = {
+            host: 'datamall2.mytransport.sg', // here only the domain name
+            port: 80,
+            path: '/ltaodataservice/BusArrival?BusStopID=' + resp, // the rest of the url with parameters if needed
+            method: 'GET',
+            headers: {
+                AccountKey: '***REMOVED***'
             }
-            // photo can be: a file path, a stream or a Teleram file_id
-            bot.sendMessage(chatId, busObj,{"parse_mode":"Markdown"});
-            console.info('\n\nCall completed');
+        };
 
-            // your code here if you want to use the results !
+        var responseData = "";
+        console.log("Starting Get");
+        // do the GET request
+        var reqGet = http.request(options, function(res) {
+            console.log("statusCode: ", res.statusCode);
+            // uncomment it for header details
+            res.on('data', function(d) {
+                console.info('GET result:\n');
+                responseData += d;
+            });
+
+            res.on('end', function() {
+                // console.log(req.data);
+                console.log(responseData);
+                responseData = JSON.parse(responseData);
+                var busObj = "";
+                for (var i = 0; i < responseData['Services'].length; i++) {
+                    var bus = responseData['Services'][i];
+
+                    busObj += "=============================\n"
+                    busObj += "*Service Number : " + bus['ServiceNo'] + "*\n";
+
+                    if (bus['Status'] === 'In Operation') {
+                        // first bus
+                        var nextBus = bus['NextBus'];
+                        var minutes = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
+                        busObj += "Next Bus : ";
+                        busObj += minutes <= 1 ? "now\n" : minutes + " minutes\n";
+
+                        // second bus
+                        nextBus = bus['SubsequentBus'];
+                        minutes = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
+                        busObj += "Next Bus : ";
+                        busObj += minutes <= 1 ? "now\n" : minutes + " minutes\n";
+
+                        // third bus
+                        nextBus = bus['SubsequentBus3'];
+                        minutes = timeDiff(new Date().getTime(), new Date(nextBus['EstimatedArrival']).getTime())
+                        busObj += "Next Bus : ";
+                        busObj += minutes <= 1 ? "now\n" : minutes + " minutes\n";
+                    } else {
+                        busObj += "Bus not in service";
+                    }
+                    busObj += "\n";
+                }
+                // photo can be: a file path, a stream or a Teleram file_id
+                bot.sendMessage(chatId, busObj, {
+                    "parse_mode": "Markdown"
+                });
+                console.info('\n\nCall completed');
+
+                // your code here if you want to use the results !
+            });
+
         });
 
-    });
+        reqGet.on('error', function(e) {
+            console.error(e);
+        });
 
-    reqGet.on('error', function(e) {
-        console.error(e);
+        reqGet.end();
     });
-
-    reqGet.end();
 });
 
 function timeDiff(nowMili, busMili) {
